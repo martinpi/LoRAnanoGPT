8266925ff30e2c31b67e335a4897621c1cee1381
Martin Pichlmair <pi@brokenrul.es>
2023-03-22 11:00:05 +0100

    use_lora = True

--- sample.py
+++ sample.py
@@ -21,6 +21,7 @@ seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
+use_lora = True
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
